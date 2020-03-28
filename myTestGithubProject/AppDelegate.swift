9110b446d1bd9563ebbd98ebbdbf123bfbc5000b
Judah Hudson <judahshudson@gmail.com>
2020-03-28 02:14:23 -0500

    Initial Commit

---
+++ myTestGithubProject/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  myTestGithubProject
+//
+//  Created by Judah Hudson on 3/28/20.
+//  Copyright © 2020 Judah Hudson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ myTestGithubProject/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  myTestGithubProject
+//
+//  Created by Judah Hudson on 3/28/20.
+//  Copyright © 2020 Judah Hudson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
